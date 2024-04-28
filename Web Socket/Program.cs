e453e6f8c5aee1a54c70d073c7ab7f58383d50a8
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-04-28 14:19:29 +0300

    sdssd

--- Web Socket/Program.cs
+++ Web Socket/Program.cs
@@ -68,25 +68,31 @@ async Task Speak(HttpContext context, WebSocket socket)
     var bite = new byte[1024];
 
     WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(bite), CancellationToken.None);
-    while (!result.CloseStatus.HasValue)
+
+
+    var inComingMessage = Encoding.UTF8.GetString(bite, 0, result.Count);
+    Console.WriteLine("Clientten gelen : " + inComingMessage);
+
+    //var rnd = new Random();
+    //var random = rnd.Next(1, 100);
+    //string message = string.Format("Numaran {0}", random.ToString());
+
+    byte[] outGoingMessage = Encoding.UTF8.GetBytes(inComingMessage);
+
+    // Sonsuz döngü 
+
+    foreach (var item in webSockets)
     {
-        var inComingMessage = Encoding.UTF8.GetString(bite, 0, result.Count);
-        Console.WriteLine("Clientten gelen : " + inComingMessage);
-
-        var rnd = new Random();
-        var random = rnd.Next(1, 100);
-        string message = string.Format("Numaran {0}", random.ToString());
-
-        byte[] outGoingMessage = Encoding.UTF8.GetBytes(message);
-
-        foreach (var item in webSockets)
-        {
-            await item.SendAsync(new ArraySegment<byte>(outGoingMessage, 0, outGoingMessage.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
-        }
-
-        result = await socket.ReceiveAsync(new ArraySegment<byte>(bite), CancellationToken.None);
-
+        item.SendAsync(new ArraySegment<byte>(outGoingMessage, 0, outGoingMessage.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
     }
-    await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 
+    //while (!result.CloseStatus.HasValue)
+    //{
+        
+    //    //result = await socket.ReceiveAsync(new ArraySegment<byte>(bite), CancellationToken.None);
+
+    //}
+   // webSockets.Remove(socket);
+   //await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+  
 }
